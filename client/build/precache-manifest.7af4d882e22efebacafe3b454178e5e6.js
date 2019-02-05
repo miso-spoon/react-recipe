1a2810db5b24cdde97cc8ec2154edaaaeb8674ce
Nathan Lui <nlui@intertech.com>
2019-02-05 09:40:32 -0600

    v4

---
+++ client/build/precache-manifest.7af4d882e22efebacafe3b454178e5e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9ac4016d7a84cd8d555e",
+    "url": "/static/js/main.9ac4016d.chunk.js"
+  },
+  {
+    "revision": "0b1e6453a28ce2719d11",
+    "url": "/static/js/1.0b1e6453.chunk.js"
+  },
+  {
+    "revision": "9ac4016d7a84cd8d555e",
+    "url": "/static/css/main.af0ebb2b.chunk.css"
+  },
+  {
+    "revision": "0b1e6453a28ce2719d11",
+    "url": "/static/css/1.337d81ff.chunk.css"
+  },
+  {
+    "revision": "1ea5635ed5aa5e140718aa8a11ef4fc4",
+    "url": "/index.html"
+  }
+];
